b01b4ad0146d0ccf95854b4037ba76bc40e28e17
Mitchell Soderberg <soderber@fnal.gov>
2011-02-04 18:06:28 +0000

    Initial migration of some pieces of T962_MergeData into T962/MergeData directory.  So far just have MergeDataBeam code in here, but will soon add additional code for MINOS information.  Will delete relevant stuff from T962_MergeData once everything is in order.
    Mitch - Feb. 4, 2011

---
+++ T962/MergeData/MergeDataBeam.cxx
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////////////////
+//
+//   MergeData_Beam Class
+//
+////////////////////////////////////////////////////////////////////////
+#include "art/Framework/Core/Event.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Persistency/Common/Handle.h"
+#include "art/Persistency/Common/Ptr.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <dirent.h>
+#include <time.h>
+#include <stdio.h>
+#include <cstdlib>
+
+#include "T962/MergeData/MergeDataBeam.h"
+
+namespace merge{
+
+  //-------------------------------------------------
+  MergeDataBeam::MergeDataBeam(fhicl::ParameterSet const& pset) : 
+    fdaq_modulelabel(pset.get< std::string >("daq"))
+  {
+    produces< std::vector<raw::BeamInfo> > ();
+  }
+
+  //-------------------------------------------------
+  MergeDataBeam::~MergeDataBeam()
+  {
+    
+  }
+
+  //-------------------------------------------------
+  void MergeDataBeam::produce(art::Event& evt)
+  {
+    art::Handle< std::vector<raw::DAQHeader> > daqHandle;
+    evt.getByLabel(fdaq_modulelabel,daqHandle);
+    fdaq = art::Ptr<raw::DAQHeader>(daqHandle, daqHandle->size()-1);
+       
+    std::auto_ptr<std::vector<raw::BeamInfo> > Beam_coll(new std::vector<raw::BeamInfo> );
+    raw::BeamInfo beam;
+
+    if(MergeBeam(beam)){
+      Beam_coll->push_back(beam);
+      evt.put(Beam_coll);
+    }
+  
+    return;
+  }
+  
+  //-------------------------------------------------
+  bool MergeDataBeam::MergeBeam(raw::BeamInfo& beam)
+  {
+    time_t spilltime = fdaq->GetTimeStamp();//time info. from DAQ480 software
+    spilltime = spilltime >> 32;
+    tm *timeinfo = localtime(&spilltime);//use this to get day/month/year;we should worry about users in different time zones at some point
+   
+  
+    char beamfilename[20];
+    sprintf(beamfilename,"/argoneut/data/rundata/matched/matched_%02d_%i_%d",timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_year+1900);
+    std::ifstream beamfile(beamfilename);
+    if(!beamfile.is_open()){
+       mf::LogDebug ("badfile") << "MergeBeam could not open file named " << beamfilename ;
+       return false;
+    }
+  
+    long long int tms;
+    double tor101;
+    double tortgt;
+    double trtgtd;
+    std::string first,k;
+    int event,run;
+  
+    bool foundbeaminfo = false;
+  
+    while(getline(beamfile,k)){
+    
+       std::istringstream ins;
+       ins.clear();
+       ins.str(k);
+       ins>>first>>run>>event>>tms>> tor101 >> tortgt >> trtgtd;
+       //  std::cout<<std::setprecision(9)<<first<<"  "<<run<<"  "<<event<<"  "<<tms<<" "<<tor101<<"  "<< tortgt <<"  "<< trtgtd <<std::endl;
+   
+       if((fdaq->GetRun()==run)&&(fdaq->GetEvent()==event ) ){ 
+          foundbeaminfo=true;
+          beam.SetTOR101(tor101);
+          beam.SetTORTGT(tortgt);
+          beam.SetTRTGTD(trtgtd);
+          beam.SetT_MS(tms);
+          break;
+       }
+    }
+  
+    beamfile.close();
+    
+    return foundbeaminfo;
+  
+  }
+
+}//namespace merge
---
+++ T962/MergeData/MergeDataBeam.h
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Merge POT information into DAQ480 data,
+// if this info. exists for a given event.
+// Kinga.partyka@yale.edu
+// msoderbe@syr.edu
+//
+////////////////////////////////////////////////////////////////////////
+#ifndef MERGEDATABEAM_H
+#define MERGEDATABEAM_H
+
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Persistency/Common/Ptr.h"
+
+#include "RawData/BeamInfo.h"
+#include "RawData/DAQHeader.h"
+
+#include <string>
+
+namespace merge {
+
+  class MergeDataBeam : public art::EDProducer {
+
+  public:
+          
+    explicit MergeDataBeam(fhicl::ParameterSet const& pset); 
+    virtual ~MergeDataBeam();
+
+    void produce(art::Event& evt);
+  
+  private: 
+
+    bool MergeBeam(raw::BeamInfo& beam);                 ///method to merge beam data
+    
+    art::Ptr<raw::DAQHeader> fdaq;
+    std::string  fdaq_modulelabel;               ///< folder for input 
+     
+    
+  }; // class MergeDataBeam
+
+}
+
+#endif // MERGEDATABEAM_H
