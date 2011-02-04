f872ac73e2c0cdebfe59f0b92f931a2f85b02794
Brian Rebel <brebel@fnal.gov>
2011-02-04 16:20:12 +0000

    missed these earlier this week

--- T962_MergeData/MergeData_module.cc
+++ T962_MergeData/MergeData_module.cc
@@ -5,19 +5,14 @@
 /// \version $Id: MergeData.cxx,v 1.20 2010/04/30 16:10:37 brebel Exp $
 /// \author  kinga.partyka@yale.edu
 ////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-
 // Framework includes
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "art/Framework/Core/ModuleMacros.h"
 
 #include "T962_MergeData/MergeData.h"
 
 namespace merge{
 
   // A macro required for a JobControl module.
-  DEFINE_FWK_MODULE(MergeData);
+  DEFINE_ART_MODULE(MergeData);
 
 }
--- T962_MergeData/MergeScan_module.cc
+++ T962_MergeData/MergeScan_module.cc
@@ -5,19 +5,14 @@
 /// \version $Id: MergeScan.cxx,v 1.20 2010/04/30 16:10:37 brebel Exp $
 /// \author  joshua.spitz@yale.edu
 ////////////////////////////////////////////////////////////////////////
-
-#include <iostream>
-
 // Framework includes
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "art/Framework/Core/ModuleMacros.h"
 
 #include "T962_MergeData/MergeScan.h"
 
 namespace merge{
 
   // A macro required for a JobControl module.
-  DEFINE_FWK_MODULE(MergeScan);
+  DEFINE_ART_MODULE(MergeScan);
 
 }
