931d8603cc2c3f0aec6a92cdbd91c2a3c4df779f
Mitchell Soderberg <soderber@fnal.gov>
2011-02-04 18:41:00 +0000

    Further migration of code out of T962_MergeData and into T962.  The new T962_Ojbects package is meant to house class objects that are specific to ArgoNeuT.  Please don't use this code yet...it is still in beta mode.  Once this code is suitable for use, I will delete the corresponding objects from T962_MergeData.
    Mitch - Feb. 4, 2011

---
+++ T962/T962_Objects/MINOS.h
@@ -0,0 +1,91 @@
+/////////////////////////////////////////////////////////////////////
+//
+// Definition of track data from MINOS
+//
+//  kinga.partyka@yale.edu
+//  mitchell.soderberg@yale.edu
+//
+////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <vector>
+
+namespace t962{
+
+   class MINOS  {
+      
+   public:
+      
+      MINOS();
+      ~MINOS();
+      
+      void SetMatched(int matched){fmatched = matched;}
+
+      int run(){return fminosdata.frun;}
+      int subRun(){return fminosdata.fsubRun;}
+      int snarl(){return fminosdata.fsnarl;}
+      double utc(){return fminosdata.futc;}
+      double day(){return fminosdata.fday;}
+      float trkIndex(){return fminosdata.ftrkIndex;}
+
+      int matched(){return fmatched;}
+      
+      struct MinosData{
+         
+         int frun;
+         int fsubRun;
+         int fsnarl;
+         double futc;
+         double fday;
+         float ftrkIndex;
+         float ftrkChi2;
+         double futc1;
+         double fnearns;
+         float fnearsec;
+         float ftrkE;
+         float fshwE;
+         double fcrateT0;
+         double ftmframe;
+         double fyear;
+         double foffset; 
+         float fdtnear;
+         float ftrkErange;
+         double fsgate53;
+         float ftrkqp;
+         float ftrkeqp;
+         float ftrkVtxX;
+         float ftrkVtxY;
+         float ftrkVtxZ;
+         double ftrkVtxeX;
+         double ftrkVtxeY;
+         float fcharge;
+         float ftrkmom;
+         float ftrkVtxT;
+         float ftrkTimeT0;
+         float ftrkdcosx;
+         float ftrkdcosy;
+         float ftrkdcosz;
+         double fmonth;
+         float ftrtgtd;
+         float ftortgt;
+         float ftor101;
+         float ftr101d;
+         int fgoodspill;
+         int fntrkstp;
+         float ftrkstpX[];
+         float ftrkstpY[];
+         float ftrkstpZ[];
+         float ftrkstpU[];
+         float ftrkstpV[];
+
+      } fminosdata;
+
+   private:    
+
+      int fmatched; //our variable to store whether MINOS track is matched to ArgoNeuT track
+ 
+   };   
+  
+}
+
+////////////////////////////////////////////////////////////////////////
---
+++ T962/T962_Objects/classes.h
@@ -0,0 +1,33 @@
+//
+// Build a dictionary.
+//
+// $Id: classes.h,v 1.8 2010/04/12 18:12:28  Exp $
+// $Author:  $
+// $Date: 2010/04/12 18:12:28 $
+// 
+// Original author Rob Kutschke, modified by klg
+//
+// Notes:
+// 1) The system is not able to deal with
+//    art::Wrapper<std::vector<std::string> >;
+//    The problem is somewhere inside root's reflex mechanism
+//    and Philippe Canal says that it is ( as of March 2010) a
+//    known problem.  He also says that they do not have any
+//    plans to fix it soon.  We can always work around it 
+//    by putting the string inside another object.
+
+#include "art/Persistency/Common/Wrapper.h"
+
+// nutools includes
+#include "T962/T962_Objects/Paddles.h"
+#include "T962/T962_Objects/MINOS.h"
+
+//
+// Only include objects that we would like to be able to put into the event.
+// Do not include the objects they contain internally.
+//
+template class std::vector<t962::MINOS>;
+template class std::vector<t962::Paddles>;
+
+template class art::Wrapper< std::vector<t962::MINOS>     >;
+template class art::Wrapper< std::vector<t962::Paddles>   >;
