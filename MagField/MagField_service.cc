173feeaee0703c3c887f986fddd3b137c7d8ce71
David Mckee <dmckee@fnal.gov>
2011-06-23 16:59:18 +0000

    Define a new service that supports magnetic fields in detectors.

---
+++ MagField/MagField_service.cc
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////
+/// \file MagField_service.cc
+///
+/// \version $Id$
+/// \author  dmckee@phys.ksu.edu
+////////////////////////////////////////////////////////////////////////
+
+// Framework includes
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+
+// LArSoft includes
+#include "MagField/MagField.h"
+
+namespace mag {
+
+  DEFINE_ART_SERVICE(MagField);
+
+} // namespace mag
---
+++ MagField/mag.h
@@ -0,0 +1,6 @@
+#ifndef MAG_MAG_H
+#define MAG_MAG_H
+
+#include "MagField/MagField.h"
+
+#endif // MAG_MAG_H
