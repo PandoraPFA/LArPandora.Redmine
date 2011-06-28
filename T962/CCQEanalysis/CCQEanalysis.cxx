e858eafd8edb7113bb6c46aa2729cd0aefee76f9
Kinga Partyka <kpartyka@fnal.gov>
2011-06-28 16:03:26 +0000

    making TTree, other improvements

--- T962/CCQEanalysis/CCQEanalysis.cxx
+++ T962/CCQEanalysis/CCQEanalysis.cxx
@@ -21,6 +21,8 @@
 #include "TSystem.h"
 #include "TMath.h"
 
+#include "TTree.h"
+
 #include "art/Framework/Core/Event.h" 
 #include "fhiclcpp/ParameterSet.h" 
 #include "art/Persistency/Common/Handle.h" 
@@ -53,7 +55,11 @@ t962::CCQEanalysis::CCQEanalysis(fhicl::ParameterSet const& pset) :
   fGenieGenModuleLabel      (pset.get< std::string >("GenieGenModuleLabel")     ),
   fLArG4ModuleLabel         (pset.get< std::string >("LArGeantModuleLabel")     ),
   fHitsModuleLabel          (pset.get< std::string >("HitsModuleLabel")         ),
-  fClusterFinderModuleLabel (pset.get< std::string >("ClusterFinderModuleLabel"))
+  fClusterFinderModuleLabel (pset.get< std::string >("ClusterFinderModuleLabel")),
+  frun(0),
+  fevent(0),
+  fbeam(0),
+  fno_primaries(100)
 {
 
 
@@ -63,31 +69,122 @@ t962::CCQEanalysis::CCQEanalysis(fhicl::ParameterSet const& pset) :
 //-------------------------------------------------
 t962::CCQEanalysis::~CCQEanalysis()
 {
-
+delete fprimaries_pdg;
+delete fEng;
+delete fPx;
+ delete fPy;
+ delete fPz;
+ delete fStartPointx;
+ delete fStartPointy;
+ delete fStartPointz;
+ delete fEndPointx;
+ delete fEndPointy;
+ delete fEndPointz;
+ 
 }
 
 void t962::CCQEanalysis::beginJob()
 {
 
   art::ServiceHandle<art::TFileService> tfs;
+  
+  //..........................................................
+  
+  fTree = tfs->make<TTree>("anatree","ccqe analysis");
+  fprimaries_pdg= new int[fno_primaries];
+  fEng= new double[fno_primaries];
+  fPx= new double[fno_primaries];
+  fPy= new double[fno_primaries];
+  fPz= new double[fno_primaries];
+  fStartPointx= new double[fno_primaries];
+  fStartPointy= new double[fno_primaries];
+  fStartPointz= new double[fno_primaries];
+  fEndPointx= new double[fno_primaries];
+  fEndPointy= new double[fno_primaries];
+  fEndPointz= new double[fno_primaries];
+  
+   fTree->Branch("beam",&fbeam,"beam/I");
+   fTree->Branch("run",&frun,"run/I");
+   fTree->Branch("event",&fevent,"event/I");
+  //......................................................
+  //fTree->Branch("pot",&pot,"pot/D");
+  //fTree->Branch("isdata",&isdata,"isdata/I");
+  // fTree->Branch("ccnc_truth",&ccnc_truth,"ccnc_truth/I");
+//   fTree->Branch("mode_truth",&mode_truth,"mode_truth/I");
+//   fTree->Branch("lep_mom_truth",&lep_mom_truth,"lep_mom_truth/D");
+//   fTree->Branch("lep_dcosx_truth",&lep_dcosx_truth,"lep_dcosx_truth/D");
+//   fTree->Branch("lep_dcosy_truth",&lep_dcosy_truth,"lep_dcosy_truth/D");
+//   fTree->Branch("lep_dcosz_truth",&lep_dcosz_truth,"lep_dcosz_truth/D");
+//   
+  //......................................................
+
+   fTree->Branch("no_primaries",&fno_primaries,"no_primaries/I");
+   
+   
+   //brian:
+  // fTree->Branch("primaries_pdg",&fprimaries_pdg);
+//   fTree->Branch("Eng",&fEng);
+  //...................................................
+  
+  
+  fTree->Branch("primaries_pdg",fprimaries_pdg,"primaries_pdg[no_primaries]/I");
+  fTree->Branch("Eng",fEng,"Eng[no_primaries]/D");
+  
+   fTree->Branch("Px",fPx,"Px[no_primaries]/D");
+ fTree->Branch("Py",fPy,"Py[no_primaries]/D");
+  fTree->Branch("Pz",fPz,"Pz[no_primaries]/D");
+  fTree->Branch("StartPointx",fStartPointx,"StartPointx[no_primaries]/D");
+  fTree->Branch("StartPointy",fStartPointy,"StartPointy[no_primaries]/D");
+  fTree->Branch("StartPointz",fStartPointz,"StartPointz[no_primaries]/D");
+  fTree->Branch("EndPointx",fEndPointx,"EndPointx[no_primaries]/D");
+  fTree->Branch("EndPointy",fEndPointy,"EndPointy[no_primaries]/D");
+  fTree->Branch("EndPointz",fEndPointz,"EndPointz[no_primaries]/D");
+  
+//   
+  
+  
+  // fTree->Branch("theta_truth",&Mu_theta_truth,"Mu_theta_truth/D");
+//   fTree->Branch("phi_truth",&Mu_phi_truth,"Mu_phi_truth/D");
+//   fTree->Branch("charge_truth",&Mu_charge_truth,"Mu_charge_truth/I");
+//   
+  
+  
+  
+  
+  
+  
+  
+    //..........................................................
+
 Mu_theta=tfs->make<TH1F>("Mu_theta","Muon theta angle", 360,0 ,360);
 Mu_phi=tfs->make<TH1F>("Mu_phi","Muon phi angle", 360,0 ,360);
 mu_charge=tfs->make<TH1F>("mu_charge","Muon charge", 4,-2 ,2);
 No_protons_in_event=tfs->make<TH1F>("No_protons_in_event","No of protons in each CCQE event", 10,0 ,10);
 No_particles_in_event=tfs->make<TH1F>("No_particles_in_event","No of particles in each CCQE event (excluding neutrons), track>1 cm", 20,0 ,20);
 P_containment=tfs->make<TH1F>("P_containment","Proton contained=1, else 0", 3,-1 ,2);
+P_containment_no_cut=tfs->make<TH1F>("P_containment_no_cut","Proton contained=1, else 0, no active volume cut", 3,-1 ,2);
 proton_track_length=tfs->make<TH1F>("proton_track_length","Length of proton track", 300,0 ,150);
 
-Vertex_x=tfs->make<TH1F>("Vertex_x","vertex X coordinate value", 60,0 ,60);
+ track_l_vs_containment = tfs->make<TH1F>("track_l_vs_containment", ";Containment; Proton Track Length", 4,  -1, 3);
+
+ p_mom_vs_containment = tfs->make<TH1F>("p_mom_vs_containment", ";Containment; Proton Momentum", 4,  -1, 3);
+  
+  track_l_vs_mom = tfs->make<TH1F>("track_l_vs_mom", ";Proton Track Length; Momentum",5000, 0, 5);
+
+Vertex_x=tfs->make<TH1F>("Vertex_x","vertex X coordinate value", 70,-10 ,60);
 Vertex_y=tfs->make<TH1F>("Vertex_y","vertex Y coordinate value", 60,-30 ,30);
-Vertex_z=tfs->make<TH1F>("Vertex_z","vertex Z coordinate value", 100,0 ,100);
+Vertex_z=tfs->make<TH1F>("Vertex_z","vertex Z coordinate value", 130,-20 ,110);
 
   
 }
 
 void t962::CCQEanalysis::analyze(const art::Event& evt)
 {
-  
+
+   frun = evt.run();
+   fevent = evt.id().event();
+   fbeam=1; //1 for nu mode, -1 for anti-nu mode
+   
   std::cout << "run    : " << evt.run() << std::endl;
   std::cout << "event  : " << evt.id().event() << std::endl;
   //----------------------------------------------------------------
@@ -153,11 +250,18 @@ double MC_Total_Eng=0;
     //First determine what kind of event we are dealing with, and select only the ones you want:
  //..........................................................
  int no_protons=0;
+ int mode=-999;
+ int ccnc=-999;
  
  
  for( unsigned int i = 0; i < mclist.size(); ++i ){
     art::Ptr<simb::MCTruth> mc(mclist[i]);
     simb::MCParticle neut(mc->GetParticle(i));
+    
+    ccnc=mclist[0]->GetNeutrino().CCNC();
+    mode=mclist[0]->GetNeutrino().Mode();
+    std::cout<<"ccnc= "<<ccnc<<std::endl;
+    std::cout<<"Mode= "<<mode<<std::endl;
     
     vertex[0] =neut.Vx();
     vertex[1] =neut.Vy();
@@ -193,15 +297,12 @@ double MC_Total_Eng=0;
     }
  }
  
- std::cout<<"starting CCQE"<<std::endl;
+ 
  
   //..........................................................
   //          START	
   
-  
-  
-  
-  
+ int primary=0;
  int no_mc_particles_in_event=0;
  double mu_theta_true, mu_phi_true;
  double pion_theta_true, pion_phi_true,momentum;
@@ -210,19 +311,58 @@ double MC_Total_Eng=0;
  std::string pri ("primary");
  double track_length=0;
  
+  //determine the number of primary particles from geant:
+  
+  for( unsigned int i = 0; i < geant_part.size(); ++i ){
+   
+    if(geant_part[i]->Process()==pri){
+    primary++;
+    }
+   
+   }
+  
+  fno_primaries=primary;
+  
+  
+
  
  
- 
+ // fprimaries_pdg.clear();
+//  fEng.clear();
  
  std::cout<<"geant_part.size()="<<geant_part.size()<<std::endl;
  
  for( unsigned int i = 0; i < geant_part.size(); ++i ){
    
+   if(geant_part[i]->Process()==pri){
+   
+   
+    // fprimaries_pdg.push_back(geant_part[i]->PdgCode());
+//     std::cout<<"geant_part[i]->E()= "<<geant_part[i]->E()<<std::endl;
+//     fEng.push_back(geant_part[i]->E());
+//     
+    std::cout<<"here1"<<std::endl;
+    fprimaries_pdg[i]=geant_part[i]->PdgCode();
+    std::cout<<"here2"<<std::endl;
+    fEng[i]=geant_part[i]->E();
+    fPx[i]=geant_part[i]->Px();
+    std::cout<<"here3"<<std::endl;
+    fPy[i]=geant_part[i]->Py();
+    fPz[i]=geant_part[i]->Pz();
+    
+   fStartPointx[i]=geant_part[i]->Vx();
+   fStartPointy[i]=geant_part[i]->Vy();
+   fStartPointz[i]=geant_part[i]->Vz();
+   fEndPointx[i]=geant_part[i]->EndPoint()[0];
+   fEndPointy[i]=geant_part[i]->EndPoint()[1];
+   fEndPointz[i]=geant_part[i]->EndPoint()[2];
+   
   std::cout<<"pdg= "<<geant_part[i]->PdgCode()<<" trackId= "<<geant_part[i]->TrackId()<<" mother= "<<geant_part[i]->Mother()<<" process= "<<geant_part[i]->Process()<<std::endl;
      
-     if(geant_part[i]->Process()==pri){std::cout<<" *****"<<std::endl;}
+     
 
  momentum=sqrt(pow(geant_part[i]->Px(),2)+pow(geant_part[i]->Py(),2.)+pow(geant_part[i]->Pz(),2.));
+ std::cout<<"*** mom= "<<momentum<<std::endl;
  
     startpoint_x=geant_part[i]->Vx();
     startpoint_y=geant_part[i]->Vy();
@@ -234,23 +374,29 @@ double MC_Total_Eng=0;
     track_length=sqrt(pow(endpoint_x-startpoint_x,2)+pow(endpoint_y-startpoint_y,2.)+pow(endpoint_z-startpoint_z,2.));
     std::cout<<"track_length= "<<track_length<<std::endl;
 
- //change the cut on energy?
+ 
  // count particles, exclude neutrons
-     if(geant_part[i]->PdgCode()!= 2112 && geant_part[i]->Process()==pri && geant_part[i]->E()>0.01 && momentum> 0 && geant_part[i]->PdgCode()<10000){
+     if(geant_part[i]->PdgCode()!= 2112 && geant_part[i]->Process()==pri && momentum> 0 && geant_part[i]->PdgCode()<10000){
   
+  if(geant_part[i]->PdgCode()==2212){
   proton_track_length->Fill(track_length);
+  track_l_vs_mom->Fill(track_length,momentum);
+  }
+  std::cout<<"before, no_mc_particles_in_event= "<<no_mc_particles_in_event<<std::endl;
   
-  if(track_length>1){no_mc_particles_in_event++;}
+  if(track_length>1.5){no_mc_particles_in_event++;}
   
-   std::cout<<"counting as a valid particle pdg= "<<geant_part[i]->PdgCode()<< "no_mc_particles_in_event= "<<no_mc_particles_in_event<<std::endl;
+   std::cout<<"counting as a valid particle pdg= "<<geant_part[i]->PdgCode()<< " no_mc_particles_in_event= "<<no_mc_particles_in_event<<std::endl;
    
     }
-   
-   momentum=sqrt(pow(geant_part[i]->Px(),2)+pow(geant_part[i]->Py(),2.)+pow(geant_part[i]->Pz(),2.));
     
+   //why is this written again?:
+   //momentum=sqrt(pow(geant_part[i]->Px(),2)+pow(geant_part[i]->Py(),2.)+pow(geant_part[i]->Pz(),2.));
+    
+   
     //WORK ON PROTONS:
 //..........................................................................
-    if((geant_part[i]->PdgCode()==2212) && (geant_part[i]->Process()==pri) && momentum >0 && track_length>1){
+    if((geant_part[i]->PdgCode()==2212) && (geant_part[i]->Process()==pri) && momentum >0 && track_length>1.5){
     std::cout<<"we have a proton with E="<<geant_part[i]->E()<<" and mom= "<<momentum<<std::endl;
     have_p++;
     E_p=geant_part[i]->E();
@@ -263,14 +409,31 @@ double MC_Total_Eng=0;
     if(endpoint_x >3 && endpoint_x <44 && endpoint_y >-17 && endpoint_y <17 && endpoint_z >3 && endpoint_z <87){
      std::cout<<"proton fully contained"<<std::endl;
      P_containment->Fill(1);
+     track_l_vs_containment->Fill(1.0,track_length);
+     p_mom_vs_containment->Fill(1.0,momentum);
+     
     }
     else{
     std::cout<<"proton NOT contained!!!"<<std::endl;
      P_containment->Fill(0);
+     track_l_vs_containment->Fill(0.,track_length);
+     p_mom_vs_containment->Fill(0.,momentum);
+    }
+    
+     if(endpoint_x >0 && endpoint_x <47 && endpoint_y >-20 && endpoint_y <20 && endpoint_z >0 && endpoint_z <90){
+     std::cout<<"proton fully contained"<<std::endl;
+     P_containment_no_cut->Fill(1);
+     
+    }
+    
+    else{
+    std::cout<<"proton NOT contained!!!"<<std::endl;
+     P_containment_no_cut->Fill(0);
     }
     
     
     }
+     
   //..........................................................................
   
     if((geant_part[i]->PdgCode()==13 || geant_part[i]->PdgCode()==-13) && (geant_part[i]->Process()==pri)){
@@ -303,29 +466,57 @@ double MC_Total_Eng=0;
     }
     
    
-    
+     
+  }//if primary process
  
+ } //loop thru geant particles
  
- }
+ fTree->Fill();
+
+  
  
 No_protons_in_event->Fill(have_p);
 
  No_particles_in_event->Fill(no_mc_particles_in_event);
     
+ 
 
+
+// ccnc_truth = mclist[0]->GetNeutrino().CCNC();
+// mode_truth = mclist[0]->GetNeutrino().Mode();
+// 
+//  if (mclist[0]->GetNeutrino().Lepton().P()){
+//  lep_mom_truth = mclist[0]->GetNeutrino().Lepton().P();
+//       lep_dcosx_truth = mclist[0]->GetNeutrino().Lepton().Px()/mclist[0]->GetNeutrino().Lepton().P();
+//       lep_dcosy_truth = mclist[0]->GetNeutrino().Lepton().Py()/mclist[0]->GetNeutrino().Lepton().P();
+//       lep_dcosz_truth = mclist[0]->GetNeutrino().Lepton().Pz()/mclist[0]->GetNeutrino().Lepton().P();
+//  
+//  
+//  }
+ 
+
+//fTree->Fill();
 
 have_p=0;
 no_mc_particles_in_event=0;
-geant_part.clear();
-mclist.clear();
-
-
-
-
-
+//geant_part.clear();
+//mclist.clear();
 
 
 
  
 }
 
+// void t962::CCQEanalysis::ResetVars(){
+// 
+//   run = -99999;
+//   event = -99999;
+//   ccnc_truth = -99999;
+//   mode_truth = -99999;
+//   lep_mom_truth = -99999;
+//   lep_dcosx_truth = -99999;
+//   lep_dcosy_truth = -99999;
+//   lep_dcosz_truth = -99999;
+//   
+//   
+//   }
--- T962/CCQEanalysis/CCQEanalysis.h
+++ T962/CCQEanalysis/CCQEanalysis.h
@@ -16,6 +16,7 @@
 
 class TH1F;
 class TH2F;
+class TTree;
 namespace recob { class Hit; }
 ///Cluster finding and building 
 namespace t962 {
@@ -33,7 +34,45 @@ namespace t962 {
     void beginJob();
 
   private:
- 
+  
+  
+  //void ResetVars();
+  
+    TTree* fTree;
+    //run information
+    int frun;
+    int fevent;
+    int fbeam;
+    double pot;
+    int isdata;
+     //mctruth information
+    int ccnc_truth;
+    int mode_truth;
+    double Mu_theta_truth;
+    double Mu_phi_truth;
+    int Mu_charge_truth;
+     double lep_dcosx_truth;
+    double lep_dcosy_truth;
+    double lep_dcosz_truth;
+    double lep_mom_truth;
+    //reconstructed information 
+    
+    int fno_primaries;
+  //std::vector<int> fprimaries_pdg;
+  //std::vector<double> fEng;
+  
+  int *fprimaries_pdg;
+  double *fEng;
+  double *fPx;
+  double *fPy;
+  double *fPz;
+  double *fStartPointx;
+  double *fStartPointy;
+  double *fStartPointz;
+  double *fEndPointx;
+  double *fEndPointy;
+  double *fEndPointz;
+//     
   std::string fGenieGenModuleLabel;
   std::string fLArG4ModuleLabel;
   std::string fHitsModuleLabel;
@@ -47,7 +86,12 @@ namespace t962 {
     TH1F* No_protons_in_event;
     TH1F* No_particles_in_event;
     TH1F* P_containment;
+    TH1F* P_containment_no_cut;
     TH1F* proton_track_length;
+    TH1F* track_l_vs_containment;
+    TH1F* p_mom_vs_containment;
+    TH1F* track_l_vs_mom;
+    
     TH1F* Vertex_x;
     TH1F* Vertex_y;
     TH1F* Vertex_z;
