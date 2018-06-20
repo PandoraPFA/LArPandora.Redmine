a83858c76d8230896124d3f078fea5c70d3cdc06
a-d-smith <andrewsmithweb@gmail.com>
2018-06-20 22:35:50 +0100

    Added interface to tag slices as a neutrino

--- larpandora/LArPandoraEventBuilding/Slice.h
+++ larpandora/LArPandoraEventBuilding/Slice.h
@@ -24,8 +24,9 @@ public:
      *  @param  nuScore the neutrino score from Pandora
      *  @param  nuHypothesis the slice as reconstructed under the neutrino hypothesis
      *  @param  crHypothesis the slice as reconstructed under the cosmic-ray hypothesis
+     *  @param  isNeutrino if the slice has been identified as a neutrino
      */
-    Slice(const float nuScore, const PFParticleVector &nuHypothesis, const PFParticleVector &crHypothesis);
+    Slice(const float nuScore, const PFParticleVector &nuHypothesis, const PFParticleVector &crHypothesis, const bool isNeutrino = false);
 
     /**
      *  @brief Get the neutrino score for the slice
@@ -42,10 +43,26 @@ public:
      */
     PFParticleVector GetCosmicRayHypothesis() const;
 
+    /**
+     *  @brief Check if the slice has been identified as a neutrino
+     */
+    bool IsTaggedAsNeutrino() const;
+
+    /**
+     *  @brief Tag the slice as a neutrino
+     */
+    void TagAsNeutrino();
+
+    /**
+     *  @brief Tag the slice as a cosmic
+     */
+    void TagAsCosmic();
+
 private:
-    float m_nuScore;                    ///< The neutrino score from Pandora
+    float            m_nuScore;         ///< The neutrino score from Pandora
     PFParticleVector m_nuHypothesis;    ///< The slice as reconstructed under the neutrino hypothesis
     PFParticleVector m_crHypothesis;    ///< The slice as reconstructed under the cosmic-ray hypothesis
+    bool             m_isNeutrino;      ///< If the slice has been identified as a neutrino
 };
 
 typedef std::vector<Slice> SliceVector;
