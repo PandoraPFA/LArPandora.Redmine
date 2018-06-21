7c5c7db5dcd9404282a31d07191a692216a8ad56
a-d-smith <andrewsmithweb@gmail.com>
2018-06-21 01:08:56 +0100

    Made simple neutrino id tool using pandora scores

--- larpandora/LArPandoraObjects/classes.h
+++ larpandora/LArPandoraObjects/classes.h
@@ -9,10 +9,14 @@ namespace {
     struct dictionary {
         art::Assns<larpandoraobj::PFParticleMetadata,recob::PFParticle,void> assnMetaToParticle;
         art::Assns<recob::PFParticle,larpandoraobj::PFParticleMetadata,void> assParticleToMeta;
+        art::Assns<recob::PFParticle,bool,void> assParticleToBool;
+        art::Assns<bool,recob::PFParticle,void> assBoolToParticle;
 
         art::Wrapper<larpandoraobj::PFParticleMetadata> wrapperMeta;
         art::Wrapper<std::vector<larpandoraobj::PFParticleMetadata> > wrapperMetaVector;
         art::Wrapper<art::Assns<larpandoraobj::PFParticleMetadata,recob::PFParticle,void> > wrapperAssnMetaToParticle;
         art::Wrapper<art::Assns<recob::PFParticle,larpandoraobj::PFParticleMetadata,void> > wrapperAssnParticleToMeta;
+        art::Wrapper<art::Assns<recob::PFParticle,bool,void> > wrapperAssnParticleToBool;
+        art::Wrapper<art::Assns<bool,recob::PFParticle,void> > wrapperAssnBoolToParticle;
     };
 }
